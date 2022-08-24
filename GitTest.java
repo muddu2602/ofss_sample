293817261dddd87fefb0f4e89095b8bf9db9282c
muddu2602 <abhishek.muddu26@gmail.com>
2022-08-24 15:10:57 +0530

    modified new java file

--- GitTest.java
+++ GitTest.java
@@ -1,5 +1,7 @@
 public class GitTest{
     public static void main(String args[]){
         System.out.println("GitTest is working with poll scm");
+        System.out.println("Added a new code");
+
     }
 }
